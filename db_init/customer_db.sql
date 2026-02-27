d2c8865184f3e57ccf5ea6f5b99ccded1e98f472
Xizzio <xizzio@example.com>
2026-02-27 01:24:32 +0100

    BCrypt Hashing passwords

--- db_init/customer_db.sql
+++ db_init/customer_db.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS customers (
 );
 
 INSERT INTO customers (email, username, password, admin) VALUES 
-('admin@exam.com', 'admin', 'adminpassword', true),
-('meny@exam.com', 'meny', 'menypassword', false),
-('coop@exam.com', 'coop', 'cooppassword', false),
-('kiwi@exam.com', 'kiwi', 'kiwipassword', false);
+('admin@exam.com', 'admin', '$2a$10$LtCWmKR66Br3JBt.BlIxaOCM7EWqebJ2G6I1r9ilGf1CYXfep1qwa', true), -- Password: adminpassword
+('meny@exam.com', 'meny', '$2a$10$ZqYublxRRPNDlL7Mi7Ayxu0sPKeea27K2oGJqw/R668IjCKraVCSa', false), -- Password: menypassword
+('coop@exam.com', 'coop', '$2a$10$JQXn/vrZQ9FlOFwqsPRHBObYfviXPhfQgt.tT46M/8po/Z1RVpeEu', false), -- Password: cooppassword
+('kiwi@exam.com', 'kiwi', '$2a$10$OAqMkTYbq.n8vVEokOTvjOqxsRPoX6buGsQ3ORHAA2zw8TFZ/8gOC', false); -- Password: kiwipassword
